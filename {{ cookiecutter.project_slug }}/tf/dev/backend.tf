f38b5cc73e3451218e96ec1297f1bd2a220708f7
Carl Hattingh <carl.hattingh@gmail.com>
2021-08-31 21:27:38 +1000

    correct tfstate key names

--- {{ cookiecutter.project_slug }}/tf/dev/backend.tf
+++ {{ cookiecutter.project_slug }}/tf/dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket  = "{{ cookiecutter.backend_bucket_dev }}"
-    key     = "{{ cookiecutter.project_slug }}"
+    key     = "{{ cookiecutter.project_slug }}/{{ cookiecutter.environment_dev }}/tfstate"
     region  = "{{ cookiecutter.aws_region_dev }}"
     profile = "{{ cookiecutter.aws_profile_dev }}"
   }

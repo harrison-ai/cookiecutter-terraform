25c09cd2e4dc9d636ee2a21d9728fda980843c32
Carl Hattingh <44431287+schattingh@users.noreply.github.com>
2021-10-14 22:17:08 +1100

    Bugfix/buildkite errors (#7)
    * bugfix: correct build scripts
    
    * chore: add pr templates

--- {{ cookiecutter.project_slug }}/scripts/tf-validate.sh
+++ {{ cookiecutter.project_slug }}/scripts/tf-validate.sh
@@ -2,11 +2,13 @@
 
 set -eu
 
-ENVS=$(find tf -maxdepth 1 -mindepth 1 -type d)
+ENVS=$(find tf -maxdepth 1 -mindepth 1 -type d | egrep -v '(modules|terraform)')
+
+touch .env
 
 for ENV in ${ENVS[@]}
 do
   echo "Validating environment ${ENV}"
-  docker-compose run --rm --workdir /app/${ENV} terraform terraform init -backend=false
-	docker-compose run --rm --workdir /app/${ENV} terraform terraform validate -json
+  docker-compose run --rm --workdir /app/${ENV} terraform init -backend=false
+  docker-compose run --rm --workdir /app/${ENV} terraform validate -json
 done

99e437d9df197f4ed1680e91a36ec3121eb0a648
Carl Hattingh <44431287+schattingh@users.noreply.github.com>
2021-10-21 21:03:37 +1100

    feat: add date string to backend state key (#8)

--- {{ cookiecutter.project_slug }}/tf/prod/backend.tf
+++ {{ cookiecutter.project_slug }}/tf/prod/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket  = "{{ cookiecutter.backend_bucket_prod }}"
-    key     = "{{ cookiecutter.project_slug }}/{{ cookiecutter.environment_prod }}/tfstate"
+    key     = "{{ cookiecutter.project_slug }}-{% now 'utc', '%Y-%m-%d' %}/{{ cookiecutter.environment_prod }}/tfstate"
     region  = "{{ cookiecutter.aws_region_prod }}"
     profile = "{{ cookiecutter.aws_profile_prod }}"
   }
